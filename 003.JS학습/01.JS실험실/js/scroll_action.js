3c429e312070ea8b40c7309bcac9635484d3bc1c
ParkSuMin0611 <71934605+ParkSuMin0611@users.noreply.github.com>
2024-04-02 17:18:58 +0900

    asd

--- 003.JS학습/01.JS실험실/js/scroll_action.js
+++ 003.JS학습/01.JS실험실/js/scroll_action.js
@@ -10,6 +10,12 @@ const myFn = {
 
   // 이벤트셋팅함수
   addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  //바운딩함수
+  getBCR: ele => ele.getBoundingClientRect().top,
+
+  //옵셋탑값 변환함수
+  getOT: ele => ele.offsetTop,
+
 }; /////// myFn 객체 /////////////
 
 /**************************************************** 
@@ -29,6 +35,15 @@ const myFn = {
     (4) document.querySelector('html').scrollTop
     -> 가로방향 스크롤바는 Y대신 X라는 문자를 사용함!
 
+  ((매서드명 조어 분석))
+  get : 가져와라
+  bounding: 경계선 ( -> 바운드 박스: 경계선을 가지는 직사각형)
+  Client: 보이는 화면
+  Rect: 사각형
+->>> boundingClientRect -> 보이는 화면 경계선으로 부터의 거리를 리턴해주는 매서드!
+-> 상단으로 부터의 거리: top
+-> 왼쪽으로 부터의 거리: left
+-> 경계선 아래쪽: 양수(+) / 경계선 윗쪽: 음수(-)
   3. 스크롤 등장 대상요소의 보이는 화면에서의 top값
     getBoundingClientRect().top
     -> 보이는 화면상단을 기준으로 이것보다 위로 갈경우
@@ -43,6 +58,7 @@ const myFn = {
 //1. 대상선정
 //1-2 스크롤 등장대상: .hide-el(별도의 클래스를 줌!)
 const scAct = myFn.qsa('.hide-el');
+
 // console.log('대상:',scAct);
 
 
@@ -56,12 +72,36 @@ myFn.addEvt(window,'scroll',showIt);
 //3-1 스크롤 등장액션함수
 function showIt(){
 //함수호출확인
-let temp = scAct[0].gettBoundingClientRect().top;
-console.log('첫번째대상위치:',temp);
+//class on 함수 호출하기
+//for of 문으로 호출
 
+for(let x of scAct) addOn(x);
 
+// let pos = myFn.getBCR(scAct[0]);
+// let pos1 = myFn.getBCR(scAct[1]);
+// let pos2 = myFn.getBCR(scAct[2]);
+// console.log('첫번째대상위치:',pos);
+// if(pos<500) scAct[0].classList.add('on');
+// if(pos1<500) scAct[1].classList.add('on');
+// if(pos2<500) scAct[2].classList.add('on');
 
 }/////////showIt/////////////////
+
+//스크롤 등장 기준설정: 화면 2/3
+
+const CRITARIA = window.innerHeight / 3*2;
+console.log('기준값:',CRITARIA)
+
+//class on 넣기 함수
+function addOn(ele){//ele=대상요소
+  //바운딩값 구하기
+  let bcrVal = myFn.getBCR(ele);
+
+  //기준값보다 작을때 등장
+  if (bcrVal< CRITARIA) ele.classList.add('on');
+  else ele.classList.remove('on');
+
+}///////addOn함수////////////////
 
 //[글자등장 셋팅하기]//////////////////////////////
 function showLetters(){
@@ -105,4 +145,64 @@ function showLetters(){
   setTimeout(() => {
     stage.classList.add("on");
   }, 2000);
+
+//////////////글자 스크롤 이벤트 셋팅하기 /////////////////////////
+//이벤트 대상: window
+myFn.addEvt(window,'scroll',moveTit);
+
+//기준이 되는 포스터 박스 위치 구하기
+const posTop = [];
+
+scAct.forEach((ele,idx)=>{
+  posTop[idx] = myFn.getOT(ele);
+});/////forEach
+
+//-> 특정 요소의 offsetTop값을 최상위 라인으로부터 떠러진 위치를 의미
+// 이것은 스크롤바 이동위치가 해당요소가 화면 맨 위에 걸린 상태와 같음!
+// ->>> 그러므로 화면 중간에 위치할 때의 값은 화면 높이값의 절반을 빼주면 됨
+// posTop[순번] - window.innerHeight/2 
+
+// 화면 절반크기 변수(포스터 위치에서 뺄값!)
+const gap = window.innerHeight/2;
+
+console.log('포스터 위치:',posTop)
+
+
+function moveTit(){
+  let scTop = window.scrollY;
+  console.log('타이틀 이동',scTop);
+
+//1. 맨 위 원위치 하기 : 첫번쨰 기준보다 작음
+if(scTop < posTop[0] - gap) {
+  stage.style.top = '0%';
+  stage.style.left = '50%';
+  stage.style.transition = '1s';
+}
+
+//2. 첫번째 포스터 위치 옆으로 이동
+if(scTop > posTop[0] - gap && scTop < posTop[0]) {
+  stage.style.top = '50%';
+  stage.style.left = '25%';
+  stage.style.transition = '.2s';
+}
+
+//3. 두번째 포스터 위치 옆으로 이동
+if(scTop > posTop[1] - gap && scTop < posTop[1]) {
+  stage.style.top = '70%';
+  stage.style.left = '65%';
+  stage.style.transition = '.1s';
+}
+
+//4. 세번째 포스터 위치 옆으로 이동
+if(scTop > posTop[2] - gap && scTop < posTop[2]) {
+  stage.style.top = '70%';
+  stage.style.left = '65%';
+  stage.style.transition = '.5s';
+}
+
+}///////moveTit///////////////////////////
+
+///////////moveTit함수//////////////////////
+
+
 } ///////////////////////////showLetters
