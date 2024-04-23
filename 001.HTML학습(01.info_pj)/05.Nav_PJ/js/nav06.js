090421abce6333bd5e14ae29f520d8524330e450
ParkSuMin0611 <sellbell07@gmail.com>
2024-04-23 12:28:57 +0900

    asd

--- 001.HTML학습(01.info_pj)/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습(01.info_pj)/05.Nav_PJ/js/nav06.js
@@ -1,13 +1,36 @@
 // 네비게이션 유형6 JS - nav06.js
 // 가로네비 서브별 드롭다운 전체창
 
-const myFn = {
-    // 요소선택함수 ////////
-    qs: (x) => document.querySelector(x),
-    qsEl: (el, x) => el.querySelector(x),
-    qsa: (x) => document.querySelectorAll(x),
-    qsaEl: (el, x) => el.querySelectorAll(x),
+//import영역///////////////////////////////////////////////////
+import mFn from "./my_function.js";
+import dragSlide from "./dragslide.js";
+///////////////////////////////////////////////////////////
+
+
+//[2] 기능구현 파트
+//배너리스트 세팅
+//대상: .slide
+const slideBox =  mFn.qs(".slide");
+
+//슬라이드 코드변수
+let slideCode = '';
+//1. 배너 리스트 셋팅하기
+//1) 대상: .slide
+//2) 슬라이드li 코드 만들기
+for(let i = 12; i<=24; i++){
+  let temp = (i > 13) ? (i - 13) : i;
+  //끝번호들 12, 13번 부터 시작 1부터 11까지 나열
   
-    // 이벤트셋팅함수
-    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
-  }; /////// myFn 객체 /////////////
+  //세번째 슬라이드(이미지1번)만 class on 넣기
+slideCode += `
+<li ${temp===1?'class= "on"':''}>
+<img src="./images/img_nav06/ban${temp}.png" alt="시코르 배너" />
+</li>
+`;
+}//////for
+
+//3)list코드 요소에 출력하기
+slideBox.innerHTML = slideCode;
+
+//[3]기능호출////////////////////////////////////////////////
+dragSlide("banbx");
