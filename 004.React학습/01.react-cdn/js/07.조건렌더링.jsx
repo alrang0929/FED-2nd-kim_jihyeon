11c33a198cb43d1ef487f38719e7053c68a6577e
ParkSuMin0611 <sellbell07@gmail.com>
2024-05-10 14:36:48 +0900

    asd

--- 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
+++ 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
@@ -1,6 +1,7 @@
 // 07.리액트 : 조건 렌더링  + 리스트 렌더링
 
 // 나의 함수 불러오기
+import { useState } from "react";
 import mFn from "./my_function.js";
 
 //출력대상: .root
@@ -155,9 +156,10 @@ ReactDOM.render(<WishList wList={[]} />, root[3]);
 //전달할 배열변수 /////////
 const movs = [
   {
-    year:"2020",
-    mtit:"남산의 부장들",
-    poster:"https://i.namu.wiki/i/d-g1xW3vvsfh71KCQIxl2es_i0wKyMJhkwEaXKdCgDAyhJVRb4vWA_TNnRHMksw0S6pK_nFrDITK2ISIJRuRpA.webp",
+    year: "2020",
+    mtit: "남산의 부장들",
+    poster:
+      "https://i.namu.wiki/i/d-g1xW3vvsfh71KCQIxl2es_i0wKyMJhkwEaXKdCgDAyhJVRb4vWA_TNnRHMksw0S6pK_nFrDITK2ISIJRuRpA.webp",
   },
   {
     year: "2021",
@@ -183,7 +185,6 @@ const movs = [
     poster:
       "https://i.namu.wiki/i/EWdG2Jtlu36U1-03moAiO7Hmh1waKlbB0DIEvamksSTTzWCsqDXxUiiPSdcmpAQjh_tUFOwAGhR7LX7f6U0wXQ.webp",
   },
-
 ];
 
 /* 
@@ -217,9 +218,11 @@ function MovieWishList({ wList }) {
           </ul>
           {/*영화 포스터 이미지 순서대로 만들기  */}
           {wList.map((x) => (
-            <MakeImage isrc={x.poster} 
-            ialt={x.mtit} 
-            icss={{width:"150px", padding:"0 5px"}}/>
+            <MakeImage
+              isrc={x.poster}
+              ialt={x.mtit}
+              icss={{ width: "150px", padding: "0 5px" }}
+            />
           ))}
         </div>
       )}
@@ -234,5 +237,129 @@ function MovieWishList({ wList }) {
   );
 } //////////////MovieWishList 컴포넌트
 
-//영화
+//배열값 있는 경우
 ReactDOM.render(<MovieWishList wList={movs} />, root[4]);
+//배열값 없는 경우
+ReactDOM.render(<MovieWishList wList={[]} />, root[5]);
+
+/********************************************************** 
+    4. 조건 연산자(삼항연산자)를 사용하여 조건부 랜더링하기 
+    비?집:놀이동산
+**********************************************************/
+// 명화 데이터
+const workUrl = {
+  피카소:
+    "https://m.theartin.net/web/product/big/201907/30c5a0fdd153bfdfdc8f19b2f4166fa8.jpg",
+  모네: "https://dimg.donga.com/wps/NEWS/IMAGE/2015/12/11/75316598.3.jpg",
+};
+
+//개발자가 좋아하는 그림(명화) 찍기
+
+//4-1. 작가 타이틀과 그림 찍기 컴포넌트////////////
+//구성: 작가이름 + 작품이미지
+//데이터 : 작가이름(painter)
+//         이미지 경로(작가이름의 객체 - workUrl)
+//         작품명(Wname)
+
+function MakePainting({ painter, wName }) {
+  //코드 리턴구역
+  return (
+    <div>
+      {/* 작가이름 */}
+      <h2>{painter}</h2>
+      {/* 이미지 태그 출력 */}
+      <img
+        src={workUrl[painter]}
+        alt={painter + "의 작품 " + wName}
+        style={{ width: "400px" }}
+        title={wName}
+      />
+    </div>
+  );
+} ///////MakePainting컴포넌트
+
+//4-2 전체출력 컴포넌트/////////////////
+//구성 : 전체타이틀(setTitle컴포넌트) + 변경버튼 + 작가타이틀과 그림출력(MakePainting 컴포넌트)
+//특이사항: 변경버튼 클릭시 MakePainting컴포넌트의 데이터를 변경하여 다시 출력하도록 함! (Hook사용!)
+
+function ShowLikePainter({ isChange }) {
+  //IsChange = true or false 값을 받아 기존작가를 변경하는 여부 전달변수
+  //true = 바꾸겠다 / false = 기존값 유지
+  //비?집:놀이동산 으로 그림 출력파트를 변경함!
+
+  //[Hook상태관리 변수 만들기]
+  const [result,setResult] = React.useState(isChange);
+  //const [변수명,set변수명] = useState(초기값)
+
+  ///return코드구역
+  return (
+    <React.Fragment>
+      {/* 1. 큰제목 */}
+      <SetTitle title="명화" />
+      {/* 2. 변경버튼 */}
+      <button 
+      onClick={()=>{setResult(!result)}}
+      style={{
+        fontSize:"30px",
+        padding:"10px",
+        fontWeight:"bold",
+        backgroundColor:result?"pink":"skyblue",
+        color:result?"blue":"hotpink",
+    }}>작가변경!</button>
+      {/* 3. 작품출력 */}
+      {
+        //삼항연산자로 isChange값에 따라 출력변경하기
+        //isChange =변경=> result 상태관리 변수 사용
+        result ? (
+          <MakePainting painter="피카소" wName="우는 여인" />
+        ) : (
+          <MakePainting painter="모네" wName="양산을 쓴 여인" />
+        )
+      }
+    </React.Fragment>
+  );
+} ////ShowLikePainter컴포넌트
+
+ReactDOM.render(<ShowLikePainter isChange={false} />, root[6]);
+
+
+/********************************************************* 
+    [ 리액트 훅크 : React Hook ]
+    - 일반적으로 리액트에 사용되는 변수는 처음에 
+    컴포넌트에 전달되어 초기 셋팅에 활용된다.
+    그런데 이 변수가 변경될 경우 컴포넌트의 변경이
+    자동적으로 이루어지지 않는다!
+    이런 종류의 변수 업데이트가 가상돔과 실제돔에
+    바로 반영되도록 실시간 감시역할을 하는
+    리액트의 기술내용을 담고 있는 것이 후크다!
+
+    1. 목적 : 어떤 특정 데이터가 변경될때
+        이 데이터를 할당하여 사용하고 있는 컴포넌트의
+        변경이 반영되도록 하고자 할때 후크를 사용한다!
+
+    2. 구현방법:
+        1) 노드JS SPA 개발환경에서는 상단에 import useState를 한다!
+        -> CDN 에서는 React.useState 로 사용함!
+        2) 코딩법 : useState() 메서드사용
+            배열변수 = useState(초기값)
+            (CDN) -> 배열변수 = React.useState(초기값)
+
+            ((일반형))
+            const [변수명,set변수명] = useState(초기값)
+            -> set변수명 작성시 변수명 첫글자는 대문자로 씀
+            예) 변수명 myname -> setMyname
+            -> set변수명(값) : 메서드형태로 후크변수의 값을 셋팅함!
+
+        3) 작동원리 
+            - useState에 쓴 초기값이 배열변수 첫번째변수에 할당된다!
+            - 코드에서 set변수명에 값을 할당하면
+            useState메서드가 이것을 체크하여 이 변수를 사용한
+            다른부분의 업데이트를 실행한다!
+            예컨데 컴포넌트 내부에 사용한 경우 컴포넌트가 업데이트 됨!
+        4) 사용결과
+            - 별도의 메서드 호출없이 후크 상태변수를 사용한 곳이
+            자동으로 변경될대마다 다시 갱신되는 것을 확인 할 수 있다!
+
+        -> 뷰JS의 리액티브 데이터와 매우 유사함!
+
+*****************************************************************/
