12d9711a07fb5a772f7cdc4675c99d61c290af1f
ParkSuMin0611 <sellbell07@gmail.com>
2024-05-20 16:49:40 +0900

    asd

--- 003.JS학습/JS/js6-1.date_math.js
+++ 003.JS학습/JS/js6-1.date_math.js
@@ -1,25 +1,26 @@
-//JS6-1.Date_Math객체 JS - js6-1.date_math.js
+// JS6-1.Date객체와 Math객체 JS
 
+// 나의 함수 불러오기
 import mFn from "./my_function.js";
 
-////////import area////////////////////////////////////////////
-
-//1. 요구사항: 화면에 시간을 찍으시오
-//2. 대상: .tt
+// 1. 요구사항: 화면에 시간을 찍으시오
+// 2. 대상: .tt
 const tt = mFn.qsa(".tt");
 console.log(tt);
 
-//3. 날짜/시간 찍기
+// 3. 날짜/시간찍기
 
-//함수 최초호출
+// 시간날짜함수 최초호출
 showTime();
-//시간날짜 함수 /////////////////
-//시간날짜함수 인터발 호출
+// 시간날짜함수 인터발호출
 setInterval(showTime, 1000);
 
-//월을 부르는 이름과 매칭하기
+// 시간날짜 함수 ////////
 function showTime() {
-  //월을 부르는 이름 :
+  // 1. 날짜 내장객체로 날짜객체인스턴스 생성하기
+  const today = new Date();
+
+  // 월을 부르는 이름과 매칭하기~!
   let monthName = [
     "하하월",
     "파파월",
@@ -35,40 +36,41 @@ function showTime() {
     "상그리아월",
   ];
 
-  //요일을 부르는 명칭
-  let dayName = ["월", "화", "수", "목", "금", "토", "일"];
+  // 요일을 부르는 이름과 매칭하기~!
+  let dayName = ["일", "월", "화", "수", "목", "금", "토"];
 
-  //1. 날짜 내장객체로 객체 인스턴시 생성하기
-  const today = new Date();
-  //2. 년도/월/일 찍기
+  // 2. 년도/월/일/요일찍기
   tt[0].innerText = today.getFullYear();
-  tt[1].innerText = today.getMonth();
+  // tt[1].innerText = monthName[today.getMonth()];
+  tt[1].innerText = today.getMonth() + 1;
   tt[2].innerText = today.getDate();
   tt[3].innerText = dayName[today.getDay()];
+  // 월과 요일은 배열순번으로 리턴된다!
+  // 왜냐하면 나라마다 월과 요일을 부르는 이름 다르기때문
+  // 만약 월을 숫자로 출력하려면 1만 더하면 된다!
 
-  //3. 시간 분 초 찍기
-
+  // 3. 시간/분/초찍기
   let H = today.getHours();
-  //12 이상: 오후  / 12이하: 오전
-  //24시간제로 값이 넘어오기 떄문에 12를 빼줌
-  H = H > 12 ? H - 12 : H;
-
   let M = today.getMinutes();
   let S = today.getSeconds();
 
-  tt[4].innerText = H >= 12 ? "오후" : "오전";
+  // 테스트
+  //   H = 18;
+  //   M = 2;
+  //   S = 3;
 
+  // 12이상이면 오후 아니면 오전
+  tt[4].innerText = H >= 12 ? "오후" : "오전";
+  // 24시간제로 값이 넘어오기때문에 12를 빼준다!
+  H = H > 12 ? H - 12 : H;
+  // 시/분/초에서 한자리숫자는 앞에 0 자리수 넣기
+  // 0붙이기 리턴함수
   const addZero = (x) => (x < 10 ? "0" + x : x);
 
-  //한자리 숫자는 앞에 0 자릿수 넣기
   tt[5].innerText = addZero(H);
   tt[6].innerText = addZero(M);
   tt[7].innerText = addZero(S);
-
-  //월과 요일은 배열 순번으로 리턴됨! > 나라마다 월과 요일을 부르는 이름이 다르기 때문
-  //만일 월을 숫자로 호출하려면 +1 해주면 됨!
-} //////////showTime함수///////////
-
+} /////////// showTime 함수 /////////////
 
 /*************************************** 
     [ Math 객체 ]
@@ -94,15 +96,15 @@ function showTime() {
 ***************************************/
 
 // 난수 발생시키기
-// let rdm = Math.random();
-// console.log(rdm);
+let rdm = Math.random();
+console.log(rdm);
 
 // 1~7 사이 난수발생하기
 // 방법: 난수에 발생할 최대수 곱하기 -> 올림/내림
-// rdm = rdm * 7;
-// console.log('난수*7:',rdm);
-// console.log('난수*7 내림:',Math.floor(rdm));
-// console.log('난수*7 올림:',Math.ceil(rdm));
+rdm = rdm * 7;
+console.log("난수*7:", rdm);
+console.log("난수*7 내림:", Math.floor(rdm));
+console.log("난수*7 올림:", Math.ceil(rdm));
 // -> 1부터 최대수는 올림처리 / 0부터 최대수-1은 내림처리
 
 // 중간난수는?
@@ -151,53 +153,83 @@ function showTime() {
 ****************************************/
 
 // 이미지 웹경로 배열
-const rimg = ["https://img.etnews.com/photonews/2110/1461216_20211007085904_466_0001.jpg", "https://i.namu.wiki/i/bFpYClpzX-ErL1Kdad69t09fhPPeVb2VwOB_qpH-9qxQ3Tx2Nz9zVIiLPWSMkvnVG5XWnUdQM5hHrGZ4cWneZA.webp", "https://img.imbc.com/adams/Program/202111/132804027350463581.jpg", "https://image.ytn.co.kr/general/jpg/2021/0925/202109251350012465_d.jpg"];
+const rimg = [
+  "https://img.etnews.com/photonews/2110/1461216_20211007085904_466_0001.jpg",
+  "https://i.namu.wiki/i/bFpYClpzX-ErL1Kdad69t09fhPPeVb2VwOB_qpH-9qxQ3Tx2Nz9zVIiLPWSMkvnVG5XWnUdQM5hHrGZ4cWneZA.webp",
+  "https://img.imbc.com/adams/Program/202111/132804027350463581.jpg",
+  "https://image.ytn.co.kr/general/jpg/2021/0925/202109251350012465_d.jpg",
+  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA0MDhfMTE0%2FMDAxNzEyNTQ1NzM5NzY4.CV2JuRLzs91cZq5shllTYdMDo3I1R_4OH1dI_YXcMQUg.f4SqGMxPZxxJK2pM7tWUUACuHuyCc4MagUl-lIgk8ugg.JPEG%2F0.jpg&type=sc960_832",
+  "https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5772%2F2024%2F05%2F13%2F0000035704_001_20240513113602787.jpg&type=sc960_832",
+];
 
+//배열개수
+let rimgCnt = rimg.length;
 // 1. 요구사항 : 웹경로 이미지를 화면에 넣고 랜덤하게
 // 이미지를 칼라로 약간커지게 클래스 on을 주어서 변경함!
-//2. 대상: .imbx
+
+// 2. 대상선정: .imbx
 const imbx = mFn.qs(".imbx");
 
-//3. 이미지 넣기  / map 과 forEach 비교
-// rimg.forEach(v =>{
-//     imbx.innerHTML += `<div><img src=${v} alt="드라마 제목"></div>`;
-// } );
+// 3. 이미지넣기
+// 배열만큼 이미지 넣기
 
-imbx.innerHTML = rimg.map(v =>`ㅇv>`).join('');
+// map으로 이미지넣기
+imbx.innerHTML = rimg
+  .map(
+    (v) => `
+    <div>
+        <img src="${v}" alt="드라마 포스터">
+    </div>
+`
+  )
+  .join("");
 
-//4. 랜덤 처리 대상 div 선택하ㄹ기
-const target = mFn.qsa('.imbx div');
-console.log("랜덤대상",target);
+// forEach로 배열을 먼저 사용하여 이미지 넣기
+// rimg.forEach(v=>{
+//     imbx.innerHTML +=
+//     `
+//         <div>
+//             <img src="${v}" alt="드라마 포스터">
+//         </div>
+//     `;
+// })
 
-//00 함수 호출 전에 선언해야ㅇㅇ함! 발생한 난수 저장변수
-let beNum = 999;
+// 4. 랜덤 처리 대상 div 선택하기
+const target = mFn.qsa(".imbx div");
+console.log("랜덤대상:", target);
 
-//함수최초호출
+// 전에 발생한 난수 저장변수
+let beNum = 99999;
+
+// 함수최초호출
 randomAddOn();
-//초기값은 해당 난수 범위 외의 수(처음에 통과)
-//5. 랜덤 처리함수 만들기 ////////////
-function randomAddOn(){
+// 인터발호출
+setInterval(randomAddOn, 1500);
 
-    //1. 먼저 난수를 발생시킨다!
-    let rdm = Math.floor(Math.random()*4);
-    console.log("난수:",rdm);
-    
-    //2. 전에 발생한 난수와 같으면 다시한번 난수 발생
-    //직전 난수는 bdNum에 저장함
-    while (rdm == beNum) {Math.floor(Math.random()*4);
-    console.log("다시난수:",rdm);
-    }//////////while 
+// 초기값은 해당 난수범위외의 수(처음에 통과!)
 
-    //3. while문 통과 후 결정된 난수를 직전 난수 변수에 할당
-    beNum = rdm;
+// 5. 랜덤 처리함수 만들기 ///////
+function randomAddOn() {
+  // 1. 먼저 난수를 발생시킨다!
+  let rdm = Math.floor(Math.random() * rimgCnt);
+  console.log("난수:", rdm);
 
-    //4. 랜덤으로 발생한 난수에 해당하는 div 에 class on 추가
-    //나머지는 on 제거
-    target[rdm].classList.remove('on');
-    target.forEach((ele, i)=>{
-        if(i == rdm) ele.classList.add('on');
-        else ele.classList.remove('on');
-     });
-   
-}////randomAddOn함수////////////////////
-setInterval(randomAddOn, 1000);
+  // 2. 전에 발생한 난수와 같으면 다시한번 난수발생
+  // 직전 난수는 beNum에 저장함
+  while(rdm==beNum) {
+    rdm = Math.floor(Math.random() * rimgCnt);
+    console.log("다시난수:", rdm);
+  } //////// while ////
+
+  // 3.while문 통과후 결정된 난수를 직전난수 변수에 할당
+  beNum = rdm;
+
+  // 4. 랜덤으로 발생한 난수에 해당하는 div에
+  // on클래스를 추가한다!(나머지는 on제거)
+  target.forEach((ele, i) => {
+    if (i == rdm) 
+        ele.classList.add("on");
+    else 
+        ele.classList.remove("on");
+  });
+} //////// randomAddOn 함수 ////////
