9b3e95bb4431283e652c6c3d5a4918c83411b315
ParkSuMin0611 <sellbell07@gmail.com>
2024-05-24 18:07:53 +0900

    sad

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
@@ -1,19 +1,45 @@
 //메인영역 컴포넌트
 
-export default function ItemsArea() {
+import catData from "../data/category";
+
+////import area////////////////////////////////////////////////////////////////////////
+
+export default function ItemsArea({ catName }) {
+  //catName: 카테고리 분류 이름(데이터 객체명과 동일)
+
+  //해당 카테고리 고유 데이터
+  const selData = catData[catName];
+  console.log(selData);
+
   //코드리턴구역
   return (
     <div id="main-area">
-      <main className="main-area ibx">
+      {/* 데이터 적용1: 최상위 클래스명 추가하기 */}
+      <main className={"main-area ibx " + selData.경로}>
         {/* <!-- 2-1. 카테고리 페이지 상단영역 --> */}
         <header className="cat-top-area">
           {/* <!-- 2-1-1. 서브타이틀 --> */}
-          <h2 className="cat-tit">Fashion</h2>
+          {/* 데이터적용2: 제목 넣기 */}
+          <h2 className="cat-tit">{selData.제목}</h2>
+
           {/* <!-- 2-1-2. 서브메뉴(LNB:Local Navigation Bar) --> */}
-          <nav className="lnb"></nav>
+          {
+            // 메뉴가 없음이 아닐떄만 배열 돌아 출력!
+            selData.메뉴 != "없음" && (
+              <nav className="lnb">
+                <ul>
+                  {selData.메뉴.map((v) => (
+                    <li>
+                      <a href="#">{v}</a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )
+          }
         </header>
         {/* <!-- 2-2. 카테고리 페이지 컨텐츠영역 --> */}
-        <div className="cat-cont-area living">
+        <div className="cat-cont-area">
           <section className="pt2">
             <div className="cbx bgi bg1-1">
               <h2></h2>
