93d1a25ce04f99124aa9937eb6b2e2ec0f0fae86
ParkSuMin0611 <sellbell07@gmail.com>
2024-05-28 09:48:20 +0900

    asd

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/Gallery.jsx
@@ -0,0 +1,109 @@
+// 보그 JS 갤러리 컴포넌트 - Gallery.jsx
+
+export default function Gallery() {
+  // 스와이퍼 인스턴스 생성함수 ////
+  const setSwiper = () => {
+    var swiper = new Swiper(".mySwiper", {
+      // 한 화면당 슬라이드수(아래breakpoint로 설정함)
+      slidesPerView: 1,
+      // 슬라이드 사이간격
+      spaceBetween: 10,
+      // 무한넘기기
+      loop: true,
+      // 자동넘기기
+      autoplay: {
+        // 사이간격
+        delay: 2500,
+        // 상호작용 죽이기 속성 없앰!
+        disableOnInteraction: false,
+      },
+      // 하단 블릿표시
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      // 양쪽이동버튼
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      /* 가로 사이즈별 스와이퍼 설정변경 */
+      breakpoints: {
+        // 가로 200px이상
+        200: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        // 가로 700px이상
+        700: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        // 가로 1000px이상
+        1000: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      },
+    });
+  };
+
+  // 화면 랜더링전 실행구역 /////
+  React.useLayoutEffect(() => {
+    // 스와이퍼 생성 함수 호출
+    setSwiper();
+  }); ///// useLayoutEffect //////////
+
+  // 코드 리턴구역 /////
+  return (
+    <div id="main-area">
+      <main className="main-area ibx">
+        {/* <!-- 2-1. 로그인 페이지 상단영역 --> */}
+        <header className="ctop">
+          {/* <!-- 2-1-1. 서브타이틀 --> */}
+          <h2 className="stit">Gallery</h2>
+        </header>
+        {/* <!-- 2-2. 갤러리 페이지 컨텐츠 박스 --> */}
+        <section className="scont">
+          {/* <!-- Swiper --> */}
+          <div className="swiper mySwiper">
+            <div className="swiper-wrapper">
+              <div className="swiper-slide">
+                <img src="images/people/cont2-1a.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/fashion/cont1-2b.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/people/cont2-3a.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/fashion/cont2-1b.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/fashion/cont2-2b.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/fashion/cont2-3b.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/beauty/cont1-2a.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/beauty/cont2-3b.jpg" alt="보그갤러리" />
+              </div>
+              <div className="swiper-slide">
+                <img src="images/beauty/cont1-1b.jpg" alt="보그갤러리" />
+              </div>
+            </div>
+
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div className="swiper-pagination"></div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+} /////////// Gallery 컴포넌트 //////////////
--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
@@ -1,30 +1,54 @@
-//메인영역 컴포넌트
+// 아이템 페이지영역 컴포넌트 ///////
 
+// 아이템 카테고리 데이터 불러오기
 import catData from "../data/category";
 
-////import area////////////////////////////////////////////////////////////////////////
-
 export default function ItemsArea({ catName }) {
-  //catName: 카테고리 분류 이름(데이터 객체명과 동일)
+  // catName - 카테고리 분류 이름(데이터 객체명과 동일!)
 
-  //해당 카테고리 고유 데이터
+  // 해당 카테고리의 데이터 선택하여 담기
   const selData = catData[catName];
   console.log(selData);
 
-  //코드리턴구역
+
+  // 태그 처리 구분 코드 생성함수
+  const makeCode = (data) => {
+    // console.log("배열인가?",Array.isArray(data));
+    // 배열 데이터는 태그 구성이 다름!
+    // runway 카테고리만 다름
+    if(Array.isArray(data)){
+      return(
+        <h2>
+          <small>{data[0]}</small>
+          <br />
+          {data[1]}
+        </h2>
+      );
+    } ////// if //////
+    // 배열이 아닌경우 ///
+    else{
+      return(
+        <h2>{data}</h2>
+      );
+    } ///// else //////
+
+  }; ///////// makeCode 함수 //////
+
+
+  // 코드 리턴구역 /////
   return (
     <div id="main-area">
-      {/* 데이터 적용1: 최상위 클래스명 추가하기 */}
+      {/* 데이터적용1 : 최상위 클래스명 추가하기 */}
       <main className={"main-area ibx " + selData.경로}>
         {/* <!-- 2-1. 카테고리 페이지 상단영역 --> */}
         <header className="cat-top-area">
           {/* <!-- 2-1-1. 서브타이틀 --> */}
-          {/* 데이터적용2: 제목 넣기 */}
+          {/* 데이터적용2 : 제목넣기 */}
           <h2 className="cat-tit">{selData.제목}</h2>
-
           {/* <!-- 2-1-2. 서브메뉴(LNB:Local Navigation Bar) --> */}
+          {/* 데이터적용2 : 제목넣기 */}
           {
-            // 메뉴가 없음이 아닐떄만 배열 돌아 출력!
+            // 메뉴가 "없음"이 아닐때만 배열돌아 출력함!
             selData.메뉴 != "없음" && (
               <nav className="lnb">
                 <ul>
@@ -40,30 +64,31 @@ export default function ItemsArea({ catName }) {
         </header>
         {/* <!-- 2-2. 카테고리 페이지 컨텐츠영역 --> */}
         <div className="cat-cont-area">
+          {/* 데이터적용4 : 컨텐츠 타이틀 넣기 */}
           <section className="pt2">
             <div className="cbx bgi bg1-1">
-              <h2></h2>
+              {makeCode(selData.타이틀[0])}
             </div>
             <div className="cbx bgi bg1-2">
-              <h2></h2>
+              {makeCode(selData.타이틀[1])}
             </div>
             <div className="cbx bgi bg1-3">
-              <h2></h2>
+              {makeCode(selData.타이틀[2])}
             </div>
           </section>
           <section className="pt2">
             <div className="cbx bgi bg2-1">
-              <h2></h2>
+              {makeCode(selData.타이틀[3])}
             </div>
             <div className="cbx bgi bg2-2">
-              <h2></h2>
+              {makeCode(selData.타이틀[4])}
             </div>
             <div className="cbx bgi bg2-3">
-              <h2></h2>
+              {makeCode(selData.타이틀[5])}
             </div>
           </section>
         </div>
       </main>
     </div>
   );
-} /////////서브 컴포넌트
+} ///////// ItemsArea 컴포넌트 ///////////
