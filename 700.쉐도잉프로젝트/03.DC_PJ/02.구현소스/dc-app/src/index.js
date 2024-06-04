2fceff80f90d0b2b5598324fdf8dcfb3fdc5a870
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-04 09:44:32 +0900

    asd

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -1,18 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Main from "./components/pages/Main";
+import Comics from "./components/pages/Comics";
+import Character from "./components/pages/Character";
 
-export default function MainComponent(){
+/********************************************* 
+    [ 리액트 라우터 ]
+    -> 컴포넌트를 연결하여 특정 이벤트에 모듈을
+    변경해주는 중계역할을 함
+    1. <BrowserRouter> - 라우터 Root
+    2. <Routes> - 개별 라우터를 묶어주는 역할
+    3. <Route> - 개별 라우터
+        (속성)
+            (1) path : 경로를 지정함
+                    (Link의 to속성 경로와 일치함!)
+            (2) element : 연결할 컴포넌트 지정
 
-return(
-  <>
-  <h1>ㅎㅎㅎ</h1>
-  </>
-);
+        (하위 라우트 만들기)
+            <Route path="/">
+                <Route />
+                <Route />
+                <Route />
+            </Route>
+    4. 라우터를 구성하는 컴포넌트는 자체적으로
+    내보내기 셋팅을 해야한다!
+    -> export default 라우터 컴포넌트
 
-}//////// MainComponent///
+    [ 리액트 라우터 흐름 ]
+    1. index.js 에 라우터 중앙 셋팅
+    2. Layout.jsx 레이아웃 컴포넌트를 루트로 선택
+    3. 상단영역 GNB에 <Link to> 셋팅
+    4. 메인영역에 <Outlet /> 셋팅
+    
+*********************************************/
 
-//컴포넌트 출력
-//1. 루트 객체 만들기
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-//2. 출력하기
-root.render(<MainComponent/>);
+export default function MainComponent() {
+  return (
+    // 라우터 루트로 라우터 구성시작
+    <BrowserRouter>
+      <Routes>
+        {/* 중요!!! 레이아웃 컴포넌트를 루트로 설정!
+        루트 Route 는 홀로닫지말고 반드시 다른
+        하위 라우트를 감싸도록한다!!! */}
+        <Route path="/" element={<Layout />}>
+          {/* 하위 라우트 셋팅 
+        -> path설정대신 index키워드를 쓰면 
+        첫페이지로 구성됨 -> MainArea 컴포넌트 <Outlet/>에
+        출력된다!*/}
+          <Route index element={<Main />} />
+          <Route path="character" element={<Character />} />
+          <Route path="comics" element={<Comics />} />
+        </Route>
+        {/* Layout 루트 Route로 하위 Route를 감싼다! */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+/// 컴포넌트 출력 ///
+// 먼저 root 객체 만들기
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+// 출력하기
+root.render(<MainComponent />);
