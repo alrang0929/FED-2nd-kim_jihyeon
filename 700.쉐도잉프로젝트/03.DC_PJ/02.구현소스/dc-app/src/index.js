080f5930b40f6ffcd31644add74fb423860959e7
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-05 13:11:49 +0900

    asd

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -37,6 +37,12 @@ import "../src/css/index.scss";
     2. Layout.jsx 레이아웃 컴포넌트를 루트로 선택
     3. 상단영역 GNB에 <Link to> 셋팅
     4. 메인영역에 <Outlet /> 셋팅
+    5. 라우터 연결 흐름: 
+        1) Route의 path정보셋팅
+        2) Link to 정보 클릭시 1번 정보와 대조
+        3) 1번 정보 일치시 element에 등록된 컴포넌트 로딩 
+        4) Outlet 표시 컴포넌트에 삽입
+        
     
 *********************************************/
 
@@ -56,6 +62,12 @@ export default function MainComponent() {
           <Route index element={<Main />} />
           <Route path="character" element={<Character />} />
           <Route path="comics" element={<Comics />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="series " element={<Series  />} />
+          <Route path="games" element={<Games />} />
+          <Route path="news" element={<News />} />
+          <Route path="video" element={<Video />} />
+          <Route path="board" element={<Board />} />
         </Route>
         {/* Layout 루트 Route로 하위 Route를 감싼다! */}
       </Routes>
