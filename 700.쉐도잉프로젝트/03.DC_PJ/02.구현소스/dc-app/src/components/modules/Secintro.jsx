682f690af7588e46c3cfa4c04b40a5109b7a7a26
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-07 15:21:02 +0900

    asd

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Secintro.jsx
@@ -0,0 +1,18 @@
+///DC PJ - 섹션소개모듈 / Secintro.jsx
+import React from 'react';
+import { secIntroData } from '../data/setintro';
+import "../../css/set_intro.scss";
+
+///////////////////////intro area/////////////////////
+
+
+function Secintro() {
+
+    return(
+        <div className="">
+
+        </div>
+    );
+}
+
+export default Secintro;
