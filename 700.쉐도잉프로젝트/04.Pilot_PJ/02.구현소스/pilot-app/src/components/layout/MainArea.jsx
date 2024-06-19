76c6d182a19eeef4fbcaee3ba7a1f23c6b1e7f37
ParkSuMin0611 <sellbell07@gmail.com>
2024-06-19 10:22:16 +0900

    asd

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
@@ -1,11 +1,18 @@
 //메인영역 컴포넌트 - MainArea.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import MainCont from '../pages/MainCont';
 
 /////////////import area////////////////////////
 
 function MainArea(props) {
+
+    //상태관리 변수 셋팅
+    //1. 페이지 변경 상태변수
+    const [pgName, setPgName] = useState('main');
+
+
+    //////코드 리턴구역 /////////////////////
     return (
         <>
            <MainCont/>
